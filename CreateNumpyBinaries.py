edcced2d6829cb9258f8dfa84af7f1d000b4dac7
Rahul Shah <rahul@n129-p3.eduroam.kth.se>
2019-05-13 18:15:05 +0200

    Dumb module import error

--- CreateNumpyBinaries.py
+++ CreateNumpyBinaries.py
@@ -11,7 +11,7 @@ def export_binary():
 
 def export_edf():
     edf_signals, edf_labels = EDFFileReader.read_edf_data()
-    np.save('edf_labels', edf_labels)
+    np.save('np_files/edf_labels', edf_labels)
     gc.collect()
 
     fpz_edf = edf_signals[:, 0].reshape(-1, 3000, 1)
@@ -22,14 +22,14 @@ def export_edf():
     both_edf = (both_edf - np.mean(both_edf, axis=0)) / np.std(both_edf, axis=0)
     both_edf = both_edf.reshape(-1, 3000, 1)
 
-    np.save('edf_eog', eog_edf)
-    np.save('edf_fpz', fpz_edf)
-    np.save('edf_both', both_edf)
+    np.save('np_files/edf_eog', eog_edf)
+    np.save('np_files/edf_fpz', fpz_edf)
+    np.save('np_files/edf_both', both_edf)
 
 
 def export_edfx():
     edfx_signals, edfx_labels = EDFFileReader.read_edfx_data()
-    np.save('edfx_labels', edfx_labels)
+    np.save('np_files/edfx_labels', edfx_labels)
     gc.collect()
 
     fpz_edfx = edfx_signals[:, 0].reshape(-1, 3000, 1)
@@ -40,9 +40,9 @@ def export_edfx():
     both_edfx = (both_edfx - np.mean(both_edfx, axis=0)) / np.std(both_edfx, axis=0)
     both_edfx = both_edfx.reshape(-1, 3000, 1)
 
-    np.save('edfx_eog', eog_edfx)
-    np.save('edfx_fpz', fpz_edfx)
-    np.save('edfx_both', both_edfx)
+    np.save('np_files/edfx_eog', eog_edfx)
+    np.save('np_files/edfx_fpz', fpz_edfx)
+    np.save('np_files/edfx_both', both_edfx)
 
 
 if __name__ == "__main__":
