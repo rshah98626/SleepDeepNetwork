3f4dccc183922c018912f3f19fb37c9b51042157
Rahul Shah <rahul@n129-p3.eduroam.kth.se>
2019-05-13 19:52:30 +0200

    Normalized data from -1 to 1

--- CreateNumpyBinaries.py
+++ CreateNumpyBinaries.py
@@ -1,6 +1,7 @@
 import EDFFileReader
 import gc
 import numpy as np
+import sklearn.preprocessing as sk
 
 
 def export_binary():
@@ -14,16 +15,14 @@ def export_edf():
     np.save('np_files/edf_labels', edf_labels)
     gc.collect()
 
-    fpz_edf = edf_signals[:, 0].reshape(-1, 3000, 1)
-    eog_edf = edf_signals[:, 1].reshape(-1, 3000, 1)
-    gc.collect()
-
-    both_edf = np.add(fpz_edf, eog_edf)
-    both_edf = (both_edf - np.mean(both_edf, axis=0)) / np.std(both_edf, axis=0)
-    both_edf = both_edf.reshape(-1, 3000, 1)
-
+    fpz_edf = sk.normalize(edf_signals[:, 0].reshape(-1, 1), copy=False, axis=0).reshape(-1, 3000, 1)
+    eog_edf = sk.normalize(edf_signals[:, 1].reshape(-1, 1), copy=False, axis=0).reshape(-1, 3000, 1)
+    both_edf = np.add(edf_signals[:, 0], edf_signals[:, 0])
     np.save('np_files/edf_eog', eog_edf)
     np.save('np_files/edf_fpz', fpz_edf)
+    gc.collect()
+
+    both_edf = sk.normalize(both_edf.reshape(-1, 1), copy=False, axis=0).reshape(-1, 3000, 1)
     np.save('np_files/edf_both', both_edf)
 
 
@@ -32,16 +31,14 @@ def export_edfx():
     np.save('np_files/edfx_labels', edfx_labels)
     gc.collect()
 
-    fpz_edfx = edfx_signals[:, 0].reshape(-1, 3000, 1)
-    eog_edfx = edfx_signals[:, 1].reshape(-1, 3000, 1)
-    gc.collect()
-
-    both_edfx = np.add(fpz_edfx, eog_edfx)
-    both_edfx = (both_edfx - np.mean(both_edfx, axis=0)) / np.std(both_edfx, axis=0)
-    both_edfx = both_edfx.reshape(-1, 3000, 1)
-
+    fpz_edfx = sk.normalize(edfx_signals[:, 0].reshape(-1, 1), copy=False, axis=0).reshape(-1, 3000, 1)
+    eog_edfx = sk.normalize(edfx_signals[:, 1].reshape(-1, 1), copy=False, axis=0).reshape(-1, 3000, 1)
+    both_edfx = np.add(edfx_signals[:, 0], edfx_signals[:, 0])
     np.save('np_files/edfx_eog', eog_edfx)
     np.save('np_files/edfx_fpz', fpz_edfx)
+    gc.collect()
+
+    both_edfx = sk.normalize(both_edfx.reshape(-1, 1), copy=False, axis=0).reshape(-1, 3000, 1)
     np.save('np_files/edfx_both', both_edfx)
 
 
