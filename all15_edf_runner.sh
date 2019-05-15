9b5002355f469b3efa9917d6db0e559f3379326c
Rahul Shah <rahul@n185-p169.eduroam.kth.se>
2019-05-15 16:28:06 +0200

    Pray this script works.

--- all15_edf_runner.sh
+++ all15_edf_runner.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+conda activate myenv1
+python cloud_model.py 2 fpz 128 50 > out_logs/edf_2_fpz.txt
+python cloud_model.py 2 eog 128 50 > out_logs/edf_2_eog.txt
+python cloud_model.py 2 both 128 50 > out_logs/edf_2_both.txt
 python cloud_model.py 6 fpz 128 50 > out_logs/edf_6_fpz.txt
-
+python cloud_model.py 6 eog 128 50 > out_logs/edf_6_eog.txt
+python cloud_model.py 6 both 128 50 > out_logs/edf_6_both.txt
+python cloud_model.py 5 fpz 128 50 > out_logs/edf_5_fpz.txt
+python cloud_model.py 5 eog 128 50 > out_logs/edf_5_eog.txt
+python cloud_model.py 5 both 128 50 > out_logs/edf_5_both.txt
+python cloud_model.py 4 fpz 128 50 > out_logs/edf_4_fpz.txt
+python cloud_model.py 4 eog 128 50 > out_logs/edf_4_eog.txt
+python cloud_model.py 4 both 128 50 > out_logs/edf_4_both.txt
+python cloud_model.py 3 fpz 128 50 > out_logs/edf_3_fpz.txt
+python cloud_model.py 3 eog 128 50 > out_logs/edf_3_eog.txt
+python cloud_model.py 3 both 128 50 > out_logs/edf_3_both.txt
