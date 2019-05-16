450285d55f6ffc0144e4056987c169d39a54c45e
Martin <m.kjaeer@posteo.at>
2019-05-17 01:08:46 +0200

    Changed experiment_runner.sh

--- experiment_runner.sh
+++ experiment_runner.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo Starting to cloud models
-nohup python cloud_model.py edfx 6 fpz 128 30 patient 0 experiment5 > out_logs/experiment5.txt &
-wait
-echo Finished Experiment 5
-nohup python cloud_model.py edfx 6 eog 128 30 patient 0 experiment6 > out_logs/experiment6.txt &
-wait
-echo Finished Experiment 6
-nohup python cloud_model.py edfx 6 both 128 30 patient 0 experiment7 > out_logs/experiment7.txt &
-wait
+# echo Starting to cloud models
+# nohup python cloud_model.py edfx 6 fpz 128 30 patient 0 experiment5 > out_logs/experiment5.txt &
+# wait
+# echo Finished Experiment 5
+# nohup python cloud_model.py edfx 6 eog 128 30 patient 0 experiment6 > out_logs/experiment6.txt &
+# wait
+# echo Finished Experiment 6
+# nohup python cloud_model.py edfx 6 both 128 30 patient 0 experiment7 > out_logs/experiment7.txt &
+# wait
 echo Finished Experiment 7
 nohup python cloud_model.py edfx 6 fpz 128 30 patient 1 experiment8 > out_logs/experiment8.txt &
 wait
